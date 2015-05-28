9571489007adde5429fdfa90997ed1719a29af2c
mikeh1010 <mikeh1010@gmail.com>
2015-05-28 11:52:15 -0600

    Activate custom menu

--- Test-Site/wp-content/themes/ablaze/header.php
+++ Test-Site/wp-content/themes/ablaze/header.php
@@ -12,8 +12,7 @@ Required setup items:
 4. Change permalinks - post name
 5. Install plugin: Contact Form 7
 6. Show "Custom Fields" on page editor
-7. Create functions.php file and add page thumbnails
-
+7. Create functions.php file and add page thumbnails, custom menus, featured images
  
 
 Things to add:
@@ -100,6 +99,7 @@ Things to add:
 			<div class="col-md-6">
 				<a href="/"><img src="<?php bloginfo('template_directory');?>/img/logo@2x.png" id="ablaze-logo" alt="Ablaze Energy" ></a>
 			</div>
+			
 			<div class="col-md-6" id="headericons">
 				
 					<div class="col-lg-3 col-xs-7 iconlinks" id="phone">
@@ -124,102 +124,7 @@ Things to add:
 		
 		<!--NAVIGATION BAR ----------------------------------------->
 		<section id="bluenavbar">
-			
-			
-				<nav class="container navbar navbar-default">
-					<div class="container-fluid">
-					
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						  </button>
-						  <a class="navbar-brand" href="#">Menu</a>
-						</div>
-						<!-- End Brand and menu toggle icon -->
-
-						<!-- Start of ACTUAL MENU-------------------------------------------------------------------------->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-							<ul id="parentmenu" class="container nav navbar-nav">
-							
-								<li class="parent dropdown"><a href="/solar"  >Solar</a> <!--CHANGE - active state, also <span class="caret"></span> class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" -->
-									
-									<ul class="dropdown-menu sub-menu" role="menu">
-										<li class="parent"><a href="/residential-solar">Residential</a>
-											
-											<ul class="dropdown-menu sub-sub-menu" role="menu">
-												<li><a href="/apply-now">Apply For Solar Now</a></li>
-												<li><a href="/how-it-works">How Solar Works</a></li>
-												<li><a href="/testimonials">Testimonials</a></li>
-												<li><a href="/what-solar-costs">What it Costs</a></li>												
-												<li><a href="/why-ablaze">Why Ablaze</a></li>
-												<li><a href="/solar-faq">FAQ</a></li>																								
-											</ul>
-											
-										</li>
-										
-										<li class="parent"><a href="/commercial-solar" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Commercial</a>
-											
-											<ul class="dropdown-menu sub-sub-menu" role="menu">
-												<li><a href="/commercial-solar-faq">FAQs</a></li>
-												<!--<li><a href="/commercial-solar-testimonials">Testimonials</a></li>-->												
-											</ul>
-											
-										</li>
-									</ul>
-								</li>
-								
-								<li class="parent dropdown"><a href="/lighting" >Lighting</a><!--class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"-->
-									<ul class="dropdown-menu sub-menu" role="menu">
-										<li class=""><a href="/faq">FAQ</a></li>									
-									</ul>
-								</li>
-									
-								<li class="parent dropdown"><a href="/sell-ablaze-energy" >Sell Ablaze</a> <!--class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"-->
-									<ul class="dropdown-menu sub-menu" role="menu">
-										<li class=""><a href="/benefits">Benefits</a></li>
-										<li class=""><a href="/how-to-qualify">How to Qualify</a></li>
-										<li class=""><a href="/requirements">Requirements</a></li>
-										<li class=""><a href="/sales">Sales</a></li>																			
-									</ul>
-								</li>
-								
-								<li class="parent dropdown"><a href="/about-us" >Company</a> <!--class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"-->
-									<ul class="dropdown-menu sub-menu" role="menu">
-										<li class=""><a href="/blog">Blog</a></li>
-										<li class=""><a href="/what-we-stand-for">What We Stand For</a></li>										
-									</ul>
-								</li>
-								
-								<li class=""><a href="/get-started">Contact Us</span></a></li>
-
-								<li class=" applynow"><a href="/apply-now">Apply for Solar Now</a></li>							
-								
-								
-								<!-- Original Bootstrap dropdown --------------------------------------------------
-								<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown </a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="#">Action</a></li>
-										<li><a href="#">Another action</a></li>
-										<li><a href="#">Something else here</a></li>
-										<li class="divider"></li>
-										<li><a href="#">Separated link</a></li>
-										<li class="divider"></li>
-										<li><a href="#">One more separated link</a></li>
-									</ul>
-								</li>-->
-							</ul>
-
-
-
-
-					</div>
-					</div>
-				</nav>
+			<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 			
 		</section>
 	</header>
