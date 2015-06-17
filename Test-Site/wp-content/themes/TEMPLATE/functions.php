2c8de44756854f87c78a32211f60cc1d76735382
mikeh1010 <mikeh1010@gmail.com>
2015-06-17 15:56:35 -0600

    Add post thumbnails and change "Read More" link in function.php

--- Test-Site/wp-content/themes/TEMPLATE/functions.php
+++ Test-Site/wp-content/themes/TEMPLATE/functions.php
@@ -12,7 +12,17 @@
 	//END register custom menus
 	
 	
+	//add Featured Images to pages and posts
+	add_theme_support( 'post-thumbnails' ); 
+
+
 	
+	// Replaces the excerpt "more" text by a link
+	function new_excerpt_more($more) {
+		   global $post;
+		return '... </p><a class="moretag" href="'. get_permalink($post->ID) . '">Read More</a>';
+	}
+	add_filter('excerpt_more', 'new_excerpt_more');
 	
 	
 	
