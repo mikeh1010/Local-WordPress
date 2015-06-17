03e4fc7e67f73f56fed1afae7f5712d4cea0ef5b
mikeh1010 <mikeh1010@gmail.com>
2015-06-17 15:52:17 -0600

    Make a footer template

---
+++ Test-Site/wp-content/themes/TEMPLATE/footer.php
@@ -0,0 +1,26 @@
+<!--Footer for TEMPLATE theme -->
+
+</div><!-- #content .site-content -->
+
+<footer>
+
+	<!--Partner logos or any other persistent footer element -->
+	<div id="footer-stripe">
+	
+	</div><!-- #footer-stripe -->
+	
+	
+	<!-- Footer widgets -->
+	<div id="footer-widgets" class="container">
+	
+	</div><!-- #footer-widgets -->
+	
+</footer>
+
+</div> <!-- #page -->
+
+<!-- wp_footer must be right before the closing Body tag -->
+<?php wp_footer(); ?> 
+</body>
+
+</html>
